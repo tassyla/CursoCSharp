88d8295219a3b7e7909207bae9f67dd8f8ffadbe
Tássyla Lima <tassyla.lima@dtidigital.com.br>
2022-04-12 19:44:05 -0300

    Exercício de Notação Ponto

--- CursoCSharp/Program.cs
+++ CursoCSharp/Program.cs
@@ -12,6 +12,10 @@ namespace CursoCSharp {
             var central = new CentralDeExercicios(new Dictionary<string, Action>() {
                 {"Primeiro Programa - Fundamentos", PrimeiroPrograma.Executar},
                 {"Comentários - Fundamentos", Comentarios.Executar},
+                {"Variáveis e Constantes - Fundamentos", VariaveisEConstantes.Executar},
+                {"Inferência - Fundamentos", Inferencia.Executar},
+                {"Interpolação - Fundamentos", Interpolacao.Executar},
+                {"Notação Ponto - Fundamentos", NotacaoPonto.Executar},
             });
 
             central.SelecionarEExecutar();

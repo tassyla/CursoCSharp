92c5ec199e134f88240c706e6a5ed9bcdc291592
Tássyla Lima <tassyla.lima@dtidigital.com.br>
2022-04-15 17:20:14 -0300

    Exercício de Lendo Dados

---
+++ CursoCSharp/Fundamentos/LendoDados.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace CursoCSharp.Fundamentos {
+
+    internal class LendoDados {
+
+        public static void Executar() {
+
+            Console.Write("Qual é seu nome? ");
+            string nome = Console.ReadLine();
+
+            Console.Write("Qual é sua idade? ");
+            int idade = int.Parse(Console.ReadLine());
+
+            Console.Write("Qual é seu salário? ");
+            double salario = double.Parse(Console.ReadLine(),
+                CultureInfo.InvariantCulture);
+
+            Console.WriteLine($"{nome}, sua idade é {idade} e seu salário é R$ {salario}.");
+        }
+    }
+}
--- CursoCSharp/Program.cs
+++ CursoCSharp/Program.cs
@@ -16,6 +16,7 @@ namespace CursoCSharp {
                 {"Inferência - Fundamentos", Inferencia.Executar},
                 {"Interpolação - Fundamentos", Interpolacao.Executar},
                 {"Notação Ponto - Fundamentos", NotacaoPonto.Executar},
+                {"Lendo Dados - Fundamentos", LendoDados.Executar},
             });
 
             central.SelecionarEExecutar();

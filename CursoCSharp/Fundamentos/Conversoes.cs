67844ff2feabaea70e72b3a33fd51ca82489e325
Tássyla Lima <tassyla.lima@dtidigital.com.br>
2022-04-15 19:42:22 -0300

    Exercício Conversões

---
+++ CursoCSharp/Fundamentos/Conversoes.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoCSharp.Fundamentos {
+
+    internal class Conversoes {
+
+        public static void Executar() {
+
+            int inteiro = 10;
+            double quebrado = inteiro;
+            Console.WriteLine(quebrado);
+
+            double nota = 9.7;
+            int notaTruncada = (int) nota;
+            Console.WriteLine("Nota truncada: " + notaTruncada);
+
+            Console.Write("Digite sua idade: ");
+            string idadeString = Console.ReadLine();
+            int idadeInteiro = int.Parse(idadeString);
+            Console.WriteLine("Idade inserida: " + idadeInteiro);
+
+            idadeInteiro = Convert.ToInt32(idadeString);
+            Console.WriteLine("Resultado: " + idadeInteiro);
+
+            Console.Write("Digite o primeiro número ");
+            string palavra = Console.ReadLine();
+            int numero;
+            int.TryParse(palavra, out numero);
+            Console.WriteLine("Resultado: " + numero);
+
+            Console.Write("Digite o primeiro número ");
+            int.TryParse(Console.ReadLine(), out int numero2);
+            Console.WriteLine("Resultado: " + numero2);
+
+        }
+    }
+}
--- CursoCSharp/Program.cs
+++ CursoCSharp/Program.cs
@@ -18,6 +18,7 @@ namespace CursoCSharp {
                 {"Notação Ponto - Fundamentos", NotacaoPonto.Executar},
                 {"Lendo Dados - Fundamentos", LendoDados.Executar},
                 {"Formatando Números - Fundamentos", FormatandoNumero.Executar},
+                {"Conversões - Fundamentos", Conversoes.Executar},
             });
 
             central.SelecionarEExecutar();

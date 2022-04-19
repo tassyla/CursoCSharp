de75a22407f3c3ca01a0c7fb17123b5c49c9709c
Tássyla Lima <tassyla.lima@dtidigital.com.br>
2022-04-19 10:40:26 -0300

    Usando Continue

--- CursoCSharp/Program.cs
+++ CursoCSharp/Program.cs
@@ -39,6 +39,7 @@ namespace CursoCSharp {
                 {"Estrutura For - Estruturas de Controle", EstruturaFor.Executar},
                 {"Estrutura ForEach - Estruturas de Controle", EstruturaForEach.Executar},
                 {"Usando Break - Estruturas de Controle", UsandoBreak.Executar},
+                {"Usando Continue - Estruturas de Controle", UsandoContinue.Executar},
 
             });
 

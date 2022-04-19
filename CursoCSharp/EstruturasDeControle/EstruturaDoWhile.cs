1304cc99b6f5aad8f6e16c349b825432ea8388a6
Tássyla Lima <tassyla.lima@dtidigital.com.br>
2022-04-18 22:18:26 -0300

    Estrutura Do While

---
+++ CursoCSharp/EstruturasDeControle/EstruturaDoWhile.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoCSharp.EstruturasDeControle {
+    internal class EstruturaDoWhile {
+
+        public static void Executar() {
+
+            string entrada;
+
+            do {
+                Console.WriteLine("Qual é seu nome?");
+                entrada = Console.ReadLine();
+
+                Console.WriteLine("Seja bem vindo(a), " + entrada);
+
+                Console.Write("Deseja continuar? (S/N): ");
+                entrada = Console.ReadLine();
+
+            } while (entrada.ToLower() == "s");
+        }
+    }
+}

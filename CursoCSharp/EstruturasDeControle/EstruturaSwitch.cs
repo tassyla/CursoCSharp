89d6d99ae4cfcca4d207c91de7f9f00dcc63a5c2
Tássyla Lima <tassyla.lima@dtidigital.com.br>
2022-04-18 21:42:35 -0300

    Estrutura Switch

---
+++ CursoCSharp/EstruturasDeControle/EstruturaSwitch.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoCSharp.EstruturasDeControle {
+
+    internal class EstruturaSwitch {
+
+        public static void Executar() {
+
+            Console.WriteLine("Avalie meu atendimento com uma nota de 1 a 5: ");
+            int.TryParse(Console.ReadLine(), out int nota);
+
+            switch (nota) {
+                case 1:
+                    Console.WriteLine("Péssimo");
+                    break;
+                case 2:
+                    Console.WriteLine("Ruim");
+                    break;
+                case 3:
+                    Console.WriteLine("Regular");
+                    break;
+                case 4:
+                    Console.WriteLine("Bom");
+                    break;
+                case 5:
+                    Console.WriteLine("Ótimo");
+                    break;
+                default:
+                    Console.WriteLine("Nota inválida");
+                    Console.WriteLine("Parabéns!");
+                    break;
+            }
+
+            Console.WriteLine("Muito obrigada por responder!");
+        }
+    }
+}

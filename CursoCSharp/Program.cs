0bb82a247d8d69a0407222879b9686588d3d7f68
Tássyla Lima <tassyla.lima@dtidigital.com.br>
2022-04-19 10:12:19 -0300

    Usando Break

--- CursoCSharp/Program.cs
+++ CursoCSharp/Program.cs
@@ -38,6 +38,7 @@ namespace CursoCSharp {
                 {"Estrutura Do While - Estruturas de Controle", EstruturaDoWhile.Executar},
                 {"Estrutura For - Estruturas de Controle", EstruturaFor.Executar},
                 {"Estrutura ForEach - Estruturas de Controle", EstruturaForEach.Executar},
+                {"Usando Break - Estruturas de Controle", UsandoBreak.Executar},
 
             });
 

a2cce36851d04137bce9c5da545289de7827b0d1
Tássyla Lima <tassyla.lima@dtidigital.com.br>
2022-04-20 08:15:01 -0300

    Métodos com retorno

--- CursoCSharp/Program.cs
+++ CursoCSharp/Program.cs
@@ -44,6 +44,8 @@ namespace CursoCSharp {
 
                 // Classes e Métodos
                 {"Membros - Classes e Métodos", Membros.Executar},
+                {"Construtores - Classes e Métodos", Construtores.Executar},
+                {"Métodos com retorno - Classes e Métodos", MetodosComRetorno.Executar},
 
             });
 

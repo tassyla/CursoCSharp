96b3592350365ca1a42fa99e6205f62b218a0f75
Tássyla Lima <tassyla.lima@dtidigital.com.br>
2022-04-23 19:26:30 -0300

    Parâmetros nomeados

--- CursoCSharp/Program.cs
+++ CursoCSharp/Program.cs
@@ -50,6 +50,7 @@ namespace CursoCSharp {
                 {"Atributos Estáticos - Classes e Métodos", AtributosEstaticos.Executar},
                 {"Desafio Atributo - Classes e Métodos", DesafioAtributo.Executar},
                 {"Params - Classes e Métodos", Params.Executar},
+                {"Parâmetros nomeados - Classes e Métodos", ParametrosNomeados.Executar},
 
             });
 

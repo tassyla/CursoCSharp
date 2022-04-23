f2badf40e988f8d88373043713f207c9fda65c44
Tássyla Lima <tassyla.lima@dtidigital.com.br>
2022-04-23 19:14:01 -0300

    Params

---
+++ CursoCSharp/ClassesEMetodos/Params.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoCSharp.ClassesEMetodos {
+
+    internal class Params {
+
+        public static void Recepcionar(params string[] pessoas) {
+
+            foreach (var pessoa in pessoas) {
+                Console.WriteLine("Olá " + pessoa);
+            }
+
+        }
+
+        public static void Executar() {
+
+            Recepcionar("Pedro", "Manu", "Roger");
+
+        }
+    }
+}

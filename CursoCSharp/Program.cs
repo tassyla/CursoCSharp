28d23d911199803116cc10c83c9ec20f8a15d1c8
Tássyla Lima <tassyla.lima@dtidigital.com.br>
2022-04-25 03:31:31 -0300

    Props

--- CursoCSharp/Program.cs
+++ CursoCSharp/Program.cs
@@ -52,6 +52,7 @@ namespace CursoCSharp {
                 {"Params - Classes e Métodos", Params.Executar},
                 {"Parâmetros nomeados - Classes e Métodos", ParametrosNomeados.Executar},
                 {"Get Set - Classes e Métodos", GetSet.Executar},
+                {"Props - Classes e Métodos", Props.Executar},
             });
 
             central.SelecionarEExecutar();
